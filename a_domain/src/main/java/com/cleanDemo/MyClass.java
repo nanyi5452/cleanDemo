4a1a767206a0a56cecb2876a3a780d91457d4b02
Wei Wang <nanyi545@126.com>
2016-03-17 16:57:49 +0800

    3-17 clean-demo

---
+++ a_domain/src/main/java/com/cleanDemo/MyClass.java
@@ -0,0 +1,7 @@
+package com.cleanDemo;
+
+/**
+ * Created by Administrator on 16-3-17.
+ */
+public class MyClass {
+}
---
+++ a_domain/src/main/java/com/cleanDemo/entity/ImageEntry.java
@@ -0,0 +1,14 @@
+package com.cleanDemo.entity;
+
+public class ImageEntry {
+
+    private String imageURL;
+    private String description;
+
+    public ImageEntry(String imageURL,String description){
+        this.imageURL=imageURL;
+        this.description=description;
+    }
+
+
+}
