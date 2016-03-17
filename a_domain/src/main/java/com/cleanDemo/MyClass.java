307b27c6152a358ad100ee943c74577ee05ccee1
e <nanyi545@126.com>
2016-03-17 23:29:46 +0800

    3-17 setup cleanDemo

--- a_domain/src/main/java/com/cleanDemo/MyClass.java
+++
@@ -1,7 +0,0 @@
-package com.cleanDemo;
-
-/**
- * Created by Administrator on 16-3-17.
- */
-public class MyClass {
-}
---
+++ ajavalib/src/main/java/com/example/coreDomain/DisplayEntry.java
@@ -0,0 +1,16 @@
+package com.example.coreDomain;
+
+/**
+ * Created by Administrator on 16-3-17.
+ */
+public class DisplayEntry {
+
+    public DisplayEntry(String imageURL,String description ){
+        this.description=description;
+        this.imageURL=imageURL;
+    }
+
+    private String imageURL;
+    private String description;
+
+}
